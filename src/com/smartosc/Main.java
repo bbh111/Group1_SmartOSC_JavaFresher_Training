bee10d0e36863332492ddc913af19c4e17e9d287
SlowV <31509114+SlowV@users.noreply.github.com>
2020-04-21 13:21:22 +0700

    Hoàn thành bài tập day1

---
+++ src/com/smartosc/Main.java
@@ -0,0 +1,98 @@
+package com.smartosc;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    static int numberCompare = 5;
+    public static void main(String[] args) {
+        // Bài 1
+         String string = "Hello World!";
+        // System.out.println(getLastCharacter(string));
+        // System.out.println(getFirstCharacter(string));
+
+        // Bài 2
+        // System.out.println(getLongNumber(new BigInteger("2"), 21024));
+
+        // Bài 3
+        // countDown(10);
+
+        // Bài 4
+        // System.out.println(convertStringToUniCode("Hello"));
+
+        // Bài 5
+        // System.out.println(getString());
+
+        // Bài 6
+        // System.out.println(takeLeft(2, string));
+
+        // Bài 7
+        // System.out.println(takeRight(2, string));
+
+        // Bài 8
+        // System.out.println(dropLeft(2, string));
+
+        // Bài 9
+        // System.out.println(dropRight(2, string));
+    }
+
+    // Bài 1
+    public static char getLastCharacter(String string) {
+        return string.charAt(string.length() - 1);
+    }
+
+    public static char getFirstCharacter(String string) {
+        return string.charAt(0);
+    }
+
+    // Bài 2
+    public static BigInteger getBigInt(BigInteger base, int exponent) {
+        return base.pow(exponent);
+    }
+
+    // Bài 3
+    public static void countDown(int number) {
+        for (int i = number; i >= 0; i--) {
+            System.out.println(number - (i + 1));
+            try {
+                Thread.sleep(1 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // bài 4
+    public static String convertStringToUniCode(String string) {
+        StringBuilder builder = new StringBuilder();
+        byte[] bytes = string.getBytes(StandardCharsets.US_ASCII);
+        for (byte aByte : bytes) {
+            builder.append(aByte);
+        }
+        return builder.toString();
+    }
+
+    public static String getString () {
+        numberCompare--;
+        if (numberCompare >= 0) {
+            return "Done";
+        }
+        return getString();
+    }
+
+    public static String takeLeft(int n, String string) {
+        return string.substring(0, n);
+    }
+
+    public static String takeRight(int n, String string) {
+        return string.substring(string.length() - (n + 1), string.length() - 1);
+    }
+
+    public static String dropLeft(int n, String s) {
+        return s.substring(n, s.length() - 1);
+    }
+
+    public static String dropRight(int n, String s) {
+        return s.substring(0, s.length() - (n + 1));
+    }
+}
