e14dcc852107ae041793b6e702a054906b0a5110
hieubb <buiba.hieu.0@gmail.com>
2020-04-29 10:33:53 +0700

    Bai tap day 5

--- src/main/java/day5/ex3/Ex3.java
+++ src/main/java/day5/ex3/Ex3.java
@@ -5,11 +5,11 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Ex3 {
-    public <T extends Comparable<T>> T maximum(List<T> list) {
+    public <T extends Comparable<T>> T maximum(List<T> list,int begin,int end) {
         T max = list.get(0);
-        for (T ele : list) {
-            if (ele.compareTo(max) > 0) {
-                max = ele;
+        for(int i = begin; i < end;i++){
+            if(list.get(i).compareTo(max) > 0){
+                max = list.get(i);
             }
         }
         return max;
@@ -19,7 +19,7 @@ public class Ex3 {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 22, 7, 9, 17};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
         Ex3 ex3 = new Ex3();
-        double temp = ex3.maximum(Arrays.asList(doubleArray));
+        double temp = ex3.maximum(Arrays.asList(doubleArray),0,3);
         System.out.println(temp);
     }
 }
